551b053ff2d3ad0cde425a1ca34d80e76672cc59
Terry Nguyen <terryhn1@uci.edu>
2020-02-01 03:34:15 -0800

    Added layout for the newFoodPost page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class userPosts extends React.Component {
+  constructor(props){
+    super(props); 
+  }
+
+  //Create a function that implements the search button
+
+  
+
+  //Add in this class the things that needs to be uploaded into the index.html later on.
+  //For each element in userPost database, then render the  
 }
 
 export default App;
--- src/newFoodPost.js
+++ src/newFoodPost.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+class newFoodPost extends React.Component {
+
+    render() {
+        //Render the survey page in order to collect information
+        
+    }
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
